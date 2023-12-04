faf46ecc00785a95a9fbc8ebc24ea2ecdd4218cb
Ngo Quang Vinh <vinhngoqv@gmail.com>
2023-12-04 21:18:31 +0900

    First commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+// scroll sections active link
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navlinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // remove toggle icon navbar when clicking to the menu-icon link
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
